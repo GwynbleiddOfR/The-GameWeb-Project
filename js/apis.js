48bd348ffce22d44d5421151d14cfe2b78bc89b7
GwynbleiddOfR <se.apileo@duocuc.cl>
2024-05-10 04:00:07 -0400

    api de gato random agregado

--- js/apis.js
+++ js/apis.js
@@ -1,27 +1,28 @@
+// api clipboard
 function copiarEmail() {
     const email = document.getElementById('mail').innerText;
     const iconoCopiar = document.getElementById('iconoCopiar');
     const alerta = document.getElementById('alerta-email-copy');
     const card = document.querySelector('.card'); // Contenedor '.card'
-  
-    navigator.clipboard.writeText(email)
-      .then(() => {
-        const iconoPos = iconoCopiar.getBoundingClientRect();
-        const cardPos = card.getBoundingClientRect();
-  
-        alerta.style.left = `${iconoPos.left - cardPos.left - 40}px`;
-        alerta.style.top = `${iconoPos.top - cardPos.top - alerta.offsetHeight - 50}px`;
-        alerta.style.display = 'block';
 
-        console.log('Correo electrónico copiado: ' + email);
-  
-        setTimeout(() => {
-          alerta.style.display = 'none';
-        }, 1000);
-      })
-      .catch(err => {
-        console.error('Error al copiar el correo electrónico:', err);
-      });
+    navigator.clipboard.writeText(email)
+        .then(() => {
+            const iconoPos = iconoCopiar.getBoundingClientRect();
+            const cardPos = card.getBoundingClientRect();
+
+            alerta.style.left = `${iconoPos.left - cardPos.left - 40}px`;
+            alerta.style.top = `${iconoPos.top - cardPos.top - alerta.offsetHeight - 50}px`;
+            alerta.style.display = 'block';
+
+            console.log('Correo electrónico copiado: ' + email);
+
+            setTimeout(() => {
+                alerta.style.display = 'none';
+            }, 1000);
+        })
+        .catch(err => {
+            console.error('Error al copiar el correo electrónico:', err);
+        });
 }
 
 function copiarTelefono() {
@@ -29,22 +30,53 @@ function copiarTelefono() {
     const iconoCopiar2 = document.getElementById('iconoCopiar2');
     const alerta = document.getElementById('alerta-tel-copy');
     const card = document.querySelector('.card');
-  
-    navigator.clipboard.writeText(telefono)
-      .then(() => {
-        const iconoPos = iconoCopiar2.getBoundingClientRect();
-        const cardPos = card.getBoundingClientRect();
-        alerta.style.left = `${iconoPos.left - cardPos.left - 40}px`;
-        alerta.style.top = `${iconoPos.top - cardPos.top - alerta.offsetHeight - 50}px`;
-        alerta.style.display = 'block';
 
-        console.log('Número de teléfono copiado: ' + telefono);
-  
-        setTimeout(() => {
-          alerta.style.display = 'none';
-        }, 1000);
-      })
-      .catch(err => {
-        console.error('Error al copiar el número de teléfono:', err);
-      });
+    navigator.clipboard.writeText(telefono)
+        .then(() => {
+            const iconoPos = iconoCopiar2.getBoundingClientRect();
+            const cardPos = card.getBoundingClientRect();
+            alerta.style.left = `${iconoPos.left - cardPos.left - 40}px`;
+            alerta.style.top = `${iconoPos.top - cardPos.top - alerta.offsetHeight - 50}px`;
+            alerta.style.display = 'block';
+
+            console.log('Número de teléfono copiado: ' + telefono);
+
+            setTimeout(() => {
+                alerta.style.display = 'none';
+            }, 1000);
+        })
+        .catch(err => {
+            console.error('Error al copiar el número de teléfono:', err);
+        });
+}
+
+//api gato random
+const url = "https://api.thecatapi.com/v1/images/search";
+const section = document.querySelector(".container");
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", getRandomCats);
+
+randomCatPhoto = (json) => {
+    let photo = json[0].url;
+    section.classList.add("cats");
+
+    let image = document.createElement("img");
+    image.src = photo;
+    image.classList.add("random_cats");
+    image.alt = photo;
+    section.appendChild(image);
+};
+
+async function getRandomCats() {
+    section.innerHTML = "";
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log("JSON:", json);
+        return randomCatPhoto(json);
+    } catch (e) {
+        console.log("Error, el gatito no se puede mostrar");
+        console.log(e);
+    }
 }
